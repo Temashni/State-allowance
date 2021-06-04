a6a86b567a6317e321f98c703abd1aec4ee0480e
Artem Mishin <artem.mishinbet98@mail.ru>
2021-06-04 19:30:14 +0400

    fixed slider

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const COUNT_SLIDE = 4;
+let indexActiveSlide = 0;
+
+function nextSlide() {
+    if (indexActiveSlide === COUNT_SLIDE-1) {
+        indexActiveSlide = 0
+    } else {
+        indexActiveSlide += 1;
+    }
+
+    updateMargin();
+}
+
+function prevSlide() {
+    if (indexActiveSlide === 0) {
+        indexActiveSlide = COUNT_SLIDE-1
+    } else {
+        indexActiveSlide -= 1;
+    }
+
+    updateMargin();
+}
+
+function updateMargin() {
+    document.getElementById("manager-slide")
+        .style.marginLeft = indexActiveSlide * 100 * -1 + '%';
+}
+
 function show(){ document.getElementById("modal").style.display="flex";
 document.getElementById("scroll").style.overflow="hidden";
 }
@@ -6,3 +34,4 @@ function unshow(){
   document.getElementById("modal").style.display="none";
   document.getElementById("scroll").style.overflow="auto";
 }
+
