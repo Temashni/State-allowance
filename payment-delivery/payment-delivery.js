e13283be8b55b5c7fb777e3334f3016916dffdf3
Artem Mishin <artem.mishinbet98@mail.ru>
2021-06-04 13:45:41 +0400

    add payment-delivery

---
+++ payment-delivery/payment-delivery.js
@@ -0,0 +1,8 @@
+function show(){ document.getElementById("modal").style.display="flex";
+document.getElementById("scroll").style.overflow="hidden";
+}
+
+function unshow(){
+  document.getElementById("modal").style.display="none";
+  document.getElementById("scroll").style.overflow="auto";
+}
