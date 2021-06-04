ea0943d91728fcf1c526607e85144e30c0b3b0be
Artem Mishin <artem.mishinbet98@mail.ru>
2021-06-04 14:10:46 +0400

    fixed error

--- payment-delivery/payment-delivery.js
+++
@@ -1,8 +0,0 @@
-function show(){ document.getElementById("modal").style.display="flex";
-document.getElementById("scroll").style.overflow="hidden";
-}
-
-function unshow(){
-  document.getElementById("modal").style.display="none";
-  document.getElementById("scroll").style.overflow="auto";
-}
